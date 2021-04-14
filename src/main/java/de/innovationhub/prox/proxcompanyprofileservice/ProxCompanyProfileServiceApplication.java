34a4689d11e733dcb9b47886695669c5438bf916
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-04-14 10:27:42 +0200

    Initial commit

---
+++ src/main/java/de/innovationhub/prox/proxcompanyprofileservice/ProxCompanyProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package de.innovationhub.prox.proxcompanyprofileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxCompanyProfileServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProxCompanyProfileServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/de/innovationhub/prox/proxcompanyprofileservice/ProxCompanyProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.innovationhub.prox.proxcompanyprofileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxCompanyProfileServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
