7f0e7119ec071e8023833ab446fcc8ed18150ab5
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-05-09 13:41:48 +0200

    Make repository transactional

--- src/main/java/de/innovationhub/prox/companyprofileservice/domain/company/CompanyRepository.java
+++ src/main/java/de/innovationhub/prox/companyprofileservice/domain/company/CompanyRepository.java
@@ -2,11 +2,13 @@ package de.innovationhub.prox.companyprofileservice.domain.company;
 
 import java.util.Optional;
 import java.util.UUID;
+import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Transactional
 public interface CompanyRepository extends CrudRepository<Company, UUID> {
   boolean existsByCreatorId(UUID uuid);
 
